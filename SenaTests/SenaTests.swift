a99b580181466d8dd82286de45c79b74cae512ea
anin <banindya10@gmail.com>
2026-09-07 11:42:14 +0700

    Initial Commit

---
+++ SenaTests/SenaTests.swift
@@ -0,0 +1,19 @@
+//
+//  SenaTests.swift
+//  SenaTests
+//
+//  Created by Benedikta Anin on 07/09/26.
+//
+
+import Testing
+@testable import Sena
+
+struct SenaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
